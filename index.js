8f30d0b41c4cbc140aaa8302f7bea3e7008ba9ce
Jean-Baptiste PIN <jean-baptiste.pin@keatonrow.com>
2014-01-31 19:34:59 -0500

    Improve boilerplate

--- index.js
+++ index.js
@@ -2,22 +2,32 @@ var express = require('express');
 var app = express();
 var cons = require('consolidate');
 
-
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.send(500, 'Something broke!');
-});
+// init view helpers
+require('./helpers/view');
 
 app.use('/public',express.static(require('path').join(__dirname, '/public')));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 
 app.engine('html', cons.dust);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+app.use(app.router);
 app.get('/', require('./routes/index.js'));
+
+app.use(function (err, req, res, next) {
+  if(!err){
+    return next();
+  }
+  console.error(err);
+  res.send(500, 'Something broke!');
+});
+
+app.use(function (req, res, next) {
+  res.send(404, 'Page not found');
+});
+
 
 app.listen(3000);
 console.log('Listening on port 3000');
